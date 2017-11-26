0c5244675819c6ba76284dd1771f3bf801f022eb
Wagner Almeida <wdmeida@gmail.com>
2017-11-25 22:24:28 -0200

    Install express-session and config send data values to form login

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,3 +1,20 @@
 module.exports.index = function(application, req, res) {
   res.render('./index');
 }
+
+module.exports.autenticar = function(application, req, res) {
+  
+  var dadosForm = req.body;
+
+  req.assert('usuario', 'Campo usuário não deve ser vazio').notEmpty();
+  req.assert('senha', 'Campo senha não deve ser vazio').notEmpty();
+
+  var erros = req.validationErrors();
+
+  if (erros) {
+    res.render('index', {validacao: erros});
+    return;
+  }
+
+  res.send('tudo ok');
+}
