83ea6d7a2e306f15b947b3c4dbdba7a7d5b900ab
Wagner Almeida <wdmeida@gmail.com>
2017-11-27 09:10:50 -0200

    Generating random values to attributes from users

--- app/controllers/jogo.js
+++ app/controllers/jogo.js
@@ -1,10 +1,17 @@
 module.exports.jogo = function(application, req, res) {
   
-  if (req.session.autorizado) {
-    res.render('jogo');    
-  } else {
+  if (!req.session.autorizado) {
     res.render('index', {validacao: {}});
-  }
+    return;    
+  } 
+
+  var usuario = req.session.usuario;
+  var casa = req.session.casa;
+
+  var connection = application.config.dbConnection;
+  var JogoDAO = new application.app.models.JogoDAO(connection);
+
+  JogoDAO.iniciaJogo(res, usuario, casa);
 }
 
 module.exports.sair = function(application, req, res) {
---
+++ app/models/JogoDAO.js
@@ -0,0 +1,37 @@
+function JogoDAO(connection) {
+  this._connection = connection();
+}
+
+JogoDAO.prototype.gerarParametros = function(usuario) {
+  this._connection.open(function(err, mongoClient) {
+    mongoClient.collection('jogo', function(err, collection) {
+      collection.insert({
+        usuario: usuario,
+        moeda: 15,
+        suditos: 10,
+        temor: Math.floor(Math.random() * 1000),
+        sabedoria: Math.floor(Math.random() * 1000),
+        comercio: Math.floor(Math.random() * 1000),
+        magia: Math.floor(Math.random() * 1000)
+      });
+
+      mongoClient.close();
+    });
+  });
+}
+
+JogoDAO.prototype.iniciaJogo = function(res, usuario, casa) {
+  this._connection.open(function(err, mongoClient) {
+    mongoClient.collection('jogo', function(err, collection) {
+      collection.find({usuario}).toArray(function(err, result) {
+        res.render('jogo', {img_casa: casa, jogo: result[0]});              
+      });
+
+      mongoClient.close();
+    });
+  });
+}
+
+module.exports = function() {
+  return JogoDAO;
+}
